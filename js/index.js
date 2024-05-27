b5d067156460f51def4640c60b270e6dc3527f44
MAngelesGB <mgutierrez6@ucol.mx>
2024-05-27 01:58:37 -0600

    corrected function of active menu items

--- js/index.js
+++ js/index.js
@@ -2,16 +2,26 @@ document.addEventListener('DOMContentLoaded', function() {
     const menuItems = document.querySelectorAll('.menu-item a');
     const currentUrl = window.location.href;
 
-    // Agregar la clase 'active' al elemento de menú 'Inicio' al cargar la página
-    menuItems.forEach(item => {
-        const linkUrl = item.getAttribute('href');
-        //remueve el active si ya existe
-        if (item.parentElement.classList.contains('active')) {
-            item.parentElement.classList.remove('active');
-        }
-        if (currentUrl.includes(linkUrl)) {
-            item.parentElement.classList.add('active');
-        }
+    // Quitar la clase 'active' de todos los elementos del menú
+    menuItems.forEach(menuItem => {
+        menuItem.parentElement.classList.remove('active');
     });
+
+    // Agregar la clase 'active' al elemento de menú 'Inicio' si la página actual es 'index.html'
+    if (currentUrl.includes('index') || currentUrl.endsWith('/')) {
+        const homeMenuItem = document.querySelector('.menu-item a[href="index.html"]');
+        if (homeMenuItem) {
+            homeMenuItem.parentElement.classList.add('active');
+        }
+    } else {
+        // Agregar la clase 'active' al elemento de menú cuyo enlace coincide con la URL actual
+        menuItems.forEach(item => {
+            const linkUrl = item.getAttribute('href');
+            if (currentUrl.includes(linkUrl)) {
+                item.parentElement.classList.add('active');
+            }
+        });
+    }
 });
+
 
